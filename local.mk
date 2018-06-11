6f864342625e816e83b901429a781ec878ed07a3
windrobust <windrobust@163.com>
2018-06-11 10:37:27 +0800

    first commit all files:local.mk, defconfig, linux-imx, uboot-imx

---
+++ local.mk
@@ -0,0 +1,4 @@
+UBOOT_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/uboot-imx
+LINUX_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/linux-imx
+LINUX_HEADERS_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/linux-imx
+
