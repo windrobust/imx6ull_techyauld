9b874c2d609b78f753f603f2874bfb0810ba4d75
windrobust <windrobust@163.com>
2018-06-12 08:47:18 +0800

    add the huawei 4g driver

--- local.mk
+++ local.mk
@@ -1,4 +1,4 @@
 UBOOT_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/uboot-imx
 LINUX_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/linux-imx
 LINUX_HEADERS_OVERRIDE_SRCDIR = /home/def/imx6ull-techyauld/linux-imx
-
+#abc
